b9657135104877d1a81694552f746f0317fce9fb
Janos Gyerik <janos@kronos>
2016-05-14 22:07:14 +0200

    should exit if virtualenv could not be loaded

--- python/common/virtualenv.sh
+++ python/common/virtualenv.sh
@@ -7,4 +7,5 @@ else
     echo virtualenv does not exist: $virtualenv >&2
     echo Create it with: virtualenv --distribute $virtualenv >&2
     echo Better yet, run ./scripts/init.sh >&2
+    exit 1
 fi
